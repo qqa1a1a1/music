516ea3c4f683e1445be1a21c7cb2bfcc28e16c96
zimo jin <631930955@qq.com>
2014-06-11 06:04:09 +0800

    background motion
    add 5 background pictures

--- scripts/selected.js
+++ scripts/selected.js
@@ -1,12 +1,4 @@
-/*
- * Selected | a collection of songs that I love
- * v0.1.18
- * also as a showcase that shows how to sync lyric with the HTML5 audio tag
- * Wayou  Apr 5th,2014
- * view on GitHub:https://github.com/wayou/selected
- * see the live site:http://wayou.github.io/selected/
- * songs used in this project are only for educational purpose. please don't distribute or sell
- */
+
 window.onload = function() {
     new Selected().init();
 };
